cd63cbc57634b25bcdf97b74b704a3c61c5e2bcd
Asendar1 <hamzahtaimour@gmail.com>
2025-03-25 15:07:16 +0300

    ogranizing. thx A.I

--- srcs/cmd_exec.c
+++ srcs/cmd_exec.c
@@ -6,11 +6,23 @@
 /*   By: hassende <hassende@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 12:48:35 by hassende          #+#    #+#             */
-/*   Updated: 2025/03/25 14:42:11 by hassende         ###   ########.fr       */
+/*   Updated: 2025/03/25 15:05:29 by hassende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+// Helper function prototypes
+static t_cmd **parse_and_prepare(char *line_read, t_cmd_path *path);
+static void process_heredocs(t_cmd **cmd);
+static void prepare_command_splits(t_cmd **cmd);
+static void execute_builtin(t_cmd *cmd, t_cmd_path *path, int stdin_backup, int stdout_backup);
+static void setup_io_redirections(t_cmd *cmd);
+static void setup_io_redirections_child(t_cmd *cmd, int *pipe_fd, int *prev_pipe, int i);
+static void execute_builtin_child(t_cmd *cmd, t_cmd_path *path);
+static void execute_command(t_cmd **cmd, t_cmd_path *path);
+static void handle_pipes(int *pipe_fd, int *prev_pipe, t_cmd **cmd, int i);
+static void wait_for_children(t_cmd_path *path);
 
 void	print_env(t_cmd_path *path)
 {
@@ -23,44 +35,67 @@ void	print_env(t_cmd_path *path)
 		i++;
 	}
 }
-
-void	exec_cmd(char *line_read, t_cmd_path *path)
+void exec_cmd(char *line_read, t_cmd_path *path)
 {
-	t_cmd	**cmd;
-	t_token	**tokens;
-	int		i;
-	int		is_child;
-	int		pipe_fd[2];
-	int		prev_pipe[2];
-	pid_t	pid;
+	t_cmd **cmd;
 
-	is_child = 0;
-	i = -1;
-	prev_pipe[0] = -1;
-	prev_pipe[1] = -1;
+	cmd = parse_and_prepare(line_read, path);
+	if (!cmd)
+		return;
+
+	process_heredocs(cmd);
+	prepare_command_splits(cmd);
+	execute_command(cmd, path);
+
+	free_cmds(cmd);
+}
+
+static t_cmd **parse_and_prepare(char *line_read, t_cmd_path *path)
+{
+	t_cmd **cmd;
+	t_token **tokens;
+
 	cmd = t_cmd_malloc(line_read);
 	if (!cmd)
-		return ;
+		return (NULL);
+
 	tokens = tokenize(line_read);
 	if (!tokens)
-		return ;
+		return (NULL);
+
 	expander(tokens, path);
 	if (!parse_token(tokens, cmd))
 	{
 		free_tokens(tokens);
-		return ;
+		return (NULL);
 	}
+
 	free_tokens(tokens);
+	return (cmd);
+}
+
+static void process_heredocs(t_cmd **cmd)
+{
+	int i;
+
+	i = -1;
 	while (cmd[++i])
 	{
 		if (cmd[i]->has_heredoc)
 			handle_heredoc(cmd[i]);
+
 		if (cmd[i]->skip_exec)
 		{
 			free_cmds(cmd);
-			return ;
+			return;
 		}
 	}
+}
+
+static void prepare_command_splits(t_cmd **cmd)
+{
+	int i;
+
 	i = -1;
 	while (cmd[++i])
 	{
@@ -68,240 +103,284 @@ void	exec_cmd(char *line_read, t_cmd_path *path)
 		if (!cmd[i]->cmd_split)
 			exit_error("Malloc failed");
 		if (cmd[i]->cmd_split[0] == NULL)
-			return ;
-		// ! each command in the pipeline should be a child proccess, run this " exit 123 | echo hi "
-		// ! you'll see that it printed hi and didn't exit bash
-		if (cmd[i] -> has_pipe || (i > 0 && cmd[i - 1]->has_pipe))
-			is_child = 1;
+			return;
+	}
+}
+
+static void execute_builtin(t_cmd *cmd, t_cmd_path *path,
+						   int stdin_backup, int stdout_backup)
+{
+	if (!ft_strncmp(cmd->cmd_split[0], "echo", 4))
+	{
+		path->exit_status = 0;
+		do_echo(cmd);
+	}
+	else if (!ft_strncmp(cmd->cmd_split[0], "exit", 4))
+	{
+		path->exit_status = do_exit(cmd);
+	}
+	else if (!ft_strncmp(cmd->cmd_split[0], "cd", 2))
+	{
+		path->exit_status = do_cd(cmd, path);
+	}
+	else if (!ft_strncmp(cmd->cmd_split[0], "export", 6))
+	{
+		path->exit_status = 0;
+		export_handle(cmd, path);
+	}
+	else if (!ft_strncmp(cmd->cmd_split[0], "env", 3))
+	{
+		path->exit_status = 0;
+		print_env(path);
+	}
+
+	if (stdin_backup != -1)
+	{
+		dup2(stdin_backup, STDIN_FILENO);
+		close(stdin_backup);
+	}
+	if (stdout_backup != -1)
+	{
+		dup2(stdout_backup, STDOUT_FILENO);
+		close(stdout_backup);
+	}
+}
+
+static void setup_io_redirections(t_cmd *cmd)
+{
+	int fd;
+
+	if (cmd->has_infile)
+	{
+		fd = open(cmd->infile, O_RDONLY);
+		if (fd == -1)
+			exit_error("File not found");
+		dup2(fd, STDIN_FILENO);
+		close(fd);
+	}
+
+	if (cmd->has_outfile)
+	{
+		fd = open(cmd->outfile, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+		if (fd == -1)
+			exit_error("File not found");
+		dup2(fd, STDOUT_FILENO);
+		close(fd);
+	}
+
+	if (cmd->has_appendfile)
+	{
+		fd = open(cmd->outfile, O_CREAT | O_WRONLY | O_APPEND, 0644);
+		if (fd == -1)
+			exit_error("File not found");
+		dup2(fd, STDOUT_FILENO);
+		close(fd);
+	}
+
+	if (cmd->has_heredoc)
+	{
+		dup2(cmd->heredoc_fd, STDIN_FILENO);
+		close(cmd->heredoc_fd);
+	}
+}
+
+static void setup_io_redirections_child(t_cmd *cmd, int *pipe_fd,
+									   int *prev_pipe, int i)
+{
+	int fd;
+
+	if (cmd->has_heredoc)
+	{
+		dup2(cmd->heredoc_fd, STDIN_FILENO);
+		close(cmd->heredoc_fd);
+	}
+
+	// Redirect output to pipe if needed
+	if (cmd->has_pipe)
+	{
+		dup2(pipe_fd[1], STDOUT_FILENO);
+		close(pipe_fd[0]);
+		close(pipe_fd[1]);
+	}
+
+	// Redirect input from previous pipe if needed
+	if (i > 0 && prev_pipe[0] != -1)
+	{
+		dup2(prev_pipe[0], STDIN_FILENO);
+		close(prev_pipe[0]);
+		close(prev_pipe[1]);
+	}
+
+	// Handle file redirections (override pipe redirections)
+	if (cmd->has_infile)
+	{
+		fd = open(cmd->infile, O_RDONLY);
+		if (fd == -1)
+			exit_error("File not found");
+		dup2(fd, STDIN_FILENO);
+		close(fd);
+	}
+
+	if (cmd->has_outfile)
+	{
+		fd = open(cmd->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (fd == -1)
+			exit_error("File not found");
+		dup2(fd, STDOUT_FILENO);
+		close(fd);
+	}
+
+	if (cmd->has_appendfile)
+	{
+		fd = open(cmd->outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
+		if (fd == -1)
+			exit_error("File not found");
+		dup2(fd, STDOUT_FILENO);
+		close(fd);
+	}
+}
+
+static void execute_builtin_child(t_cmd *cmd, t_cmd_path *path)
+{
+	if (!ft_strncmp(cmd->cmd_split[0], "echo", 4))
+	{
+		do_echo(cmd);
+		exit(0);
+	}
+	else if (!ft_strncmp(cmd->cmd_split[0], "exit", 4))
+	{
+		do_exit(cmd);
+		exit(1);
+	}
+	else if (!ft_strncmp(cmd->cmd_split[0], "cd", 2))
+	{
+		int rtn_code = do_cd(cmd, path);
+		if (rtn_code)
+			exit(rtn_code);
+		exit(0);
+	}
+	else if (!ft_strncmp(cmd->cmd_split[0], "export", 6))
+	{
+		export_handle(cmd, path);
+		exit(0);
+	}
+	else if (!ft_strncmp(cmd->cmd_split[0], "env", 3))
+	{
+		print_env(path);
+		exit(0);
+	}
+
+	// For external commands
+	setup_command(cmd, path);
+	execve(cmd->cmd_path, cmd->cmd_split, path->envp);
+	exit(127); // Command not found
+}
+
+static void handle_pipes(int *pipe_fd, int *prev_pipe, t_cmd **cmd, int i)
+{
+	// Close previous pipe if it exists
+	if (i > 0 && cmd[i-1]->has_pipe)
+	{
+		close(prev_pipe[0]);
+		close(prev_pipe[1]);
+	}
+
+	// Update pipe references - THIS IS THE FIX
+	if (cmd[i]->has_pipe)
+	{
+		// Important: Close write end of pipes when the next command doesn't use stdin
+		// This prevents SIGPIPE when the next command exits early
+		if (cmd[i+1] && (cmd[i+1]->has_infile || cmd[i+1]->has_heredoc))
+		{
+			close(pipe_fd[1]);  // Close write end early
+			prev_pipe[0] = pipe_fd[0];
+			prev_pipe[1] = -1;  // Mark as closed
+		}
+		else
+		{
+			prev_pipe[0] = pipe_fd[0];
+			prev_pipe[1] = pipe_fd[1];
+		}
+	}
+	else
+	{
+		prev_pipe[0] = -1;
+		prev_pipe[1] = -1;
+	}
+}
+
+static void execute_command(t_cmd **cmd, t_cmd_path *path)
+{
+	int i;
+	int is_child;
+	int pipe_fd[2];
+	int prev_pipe[2];
+	pid_t pid;
+	int stdin_backup;
+	int stdout_backup;
+
+	i = -1;
+	prev_pipe[0] = -1;
+	prev_pipe[1] = -1;
+
+	while (cmd[++i])
+	{
+		is_child = cmd[i]->has_pipe || (i > 0 && cmd[i-1]->has_pipe);
+
+		// Handle built-in commands in parent process
 		if (!is_child)
 		{
-			int	stdout_backup = -1;
-			int	stdin_backup = -1;
+			stdin_backup = -1;
+			stdout_backup = -1;
 
-			if ((cmd[i]->has_appendfile || cmd[i]->has_infile || cmd[i]->has_outfile) && (is_builtin(cmd[i])))
+			if ((cmd[i]->has_appendfile || cmd[i]->has_infile ||
+				 cmd[i]->has_outfile) && is_builtin(cmd[i]))
 			{
 				stdout_backup = dup(STDOUT_FILENO);
 				stdin_backup = dup(STDIN_FILENO);
+				setup_io_redirections(cmd[i]);
 			}
-			if (cmd[i]->has_infile && is_builtin(cmd[i]))
+
+			if (is_builtin(cmd[i]))
 			{
-				int fd = open(cmd[i]->infile, O_RDONLY);
-				if (fd == -1)
-					exit_error("File not found");
-				dup2(fd, STDIN_FILENO);
-				close(fd);
+				execute_builtin(cmd[i], path, stdin_backup, stdout_backup);
+				continue;
 			}
-			if (cmd[i]->has_outfile && is_builtin(cmd[i]))
-			{
-				int fd = open(cmd[i]->outfile, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-				if (fd == -1)
-					exit_error("File not found");
-				dup2(fd, STDOUT_FILENO);
-				close(fd);
-			}
-			if (cmd[i]->has_appendfile && is_builtin(cmd[i]))
-			{
-				int fd = open(cmd[i]->outfile, O_CREAT | O_WRONLY | O_APPEND, 0644);
-				if (fd == -1)
-					exit_error("File not found");
-				dup2(fd, STDOUT_FILENO);
-				close(fd);
-			}
-			if (cmd[i]->has_heredoc && is_builtin(cmd[i]))
-			{
-				dup2(cmd[i]->heredoc_fd, STDIN_FILENO);
-				close(cmd[i]->heredoc_fd);
-			}
-			if (!ft_strncmp(cmd[i]->cmd_split[0], "echo", 4))
-			{
-				do_echo(cmd[i]);
-				if (stdin_backup != -1)
-				{
-					dup2(stdin_backup, STDIN_FILENO);
-					close(stdin_backup);
-				}
-				if (stdout_backup != -1)
-				{
-					dup2(stdout_backup, STDOUT_FILENO);
-					close(stdout_backup);
-				}
-				continue ;
-			}
-			if (!ft_strncmp(cmd[i]->cmd_split[0], "exit", 4))
-			{
-				do_exit(cmd[i]);
-				if (stdin_backup != -1)
-				{
-					dup2(stdin_backup, STDIN_FILENO);
-					close(stdin_backup);
-				}
-				if (stdout_backup != -1)
-				{
-					dup2(stdout_backup, STDOUT_FILENO);
-					close(stdout_backup);
-				}
-				continue ;
-			}
-			if (!ft_strncmp(cmd[i]->cmd_split[0], "cd", 2))
-			{
-				do_cd(cmd[i], path);
-				if (stdin_backup != -1)
-				{
-					dup2(stdin_backup, STDIN_FILENO);
-					close(stdin_backup);
-				}
-				if (stdout_backup != -1)
-				{
-					dup2(stdout_backup, STDOUT_FILENO);
-					close(stdout_backup);
-				}
-				continue ;
-			}
-			// check if export not export {like this exporttttt}
-			if (!ft_strncmp(cmd[i]->cmd_split[0], "export", 6))
-			{
-				export_handle(cmd[i], path);
-				if (stdin_backup != -1)
-				{
-					dup2(stdin_backup, STDIN_FILENO);
-					close(stdin_backup);
-				}
-				if (stdout_backup != -1)
-				{
-					dup2(stdout_backup, STDOUT_FILENO);
-					close(stdout_backup);
-				}
-				continue ;
-			}
-			if (!ft_strncmp(cmd[i]->cmd_split[0], "env", 3))
-			{
-				print_env(path);
-				if (stdin_backup != -1)
-				{
-					dup2(stdin_backup, STDIN_FILENO);
-					close(stdin_backup);
-				}
-				if (stdout_backup != -1)
-				{
-					dup2(stdout_backup, STDOUT_FILENO);
-					close(stdout_backup);
-				}
-				continue ;
-			}
-			//? unset is to come.
 		}
+
+		// Create pipe if needed
 		if (cmd[i]->has_pipe)
 		{
 			if (pipe(pipe_fd) == -1)
 				exit_error("Pipe failed");
 		}
+
+		// Fork and execute command
 		pid = fork();
 		if (pid == 0)
 		{
-			if (cmd[i]->has_heredoc)
-			{
-				dup2(cmd[i]->heredoc_fd, STDIN_FILENO);
-				close(cmd[i]->heredoc_fd);
-			}
-			// Redirect output to pipe if needed
-			if (cmd[i]->has_pipe)
-			{
-				dup2(pipe_fd[1], STDOUT_FILENO);
-				close(pipe_fd[0]);
-				close(pipe_fd[1]);
-			}
-			// Redirect input from previous pipe if needed (BEFORE infile)
-			if (i > 0 && cmd[i-1]->has_pipe)
-			{
-				dup2(prev_pipe[0], STDIN_FILENO);
-				close(prev_pipe[0]);
-				close(prev_pipe[1]); // Close both ends in child
-			}
-			// Redirect input from infile (overrides previous pipe if needed)
-			if (cmd[i]->has_infile)
-			{
-				int fd = open(cmd[i]->infile, O_RDONLY);
-				if (fd == -1)
-					exit_error("File not found");
-				dup2(fd, STDIN_FILENO);
-				close(fd);
-			}
-			// Redirect output to outfile (overrides pipe if needed)
-			if (cmd[i]->has_outfile)
-			{
-				int	fd;
-				fd = open(cmd[i]->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-				if (fd == -1)
-					exit_error("File not found");
-				dup2(fd, STDOUT_FILENO);
-				close(fd);
-			}
-			if (cmd[i]->has_appendfile)
-			{
-				int fd = open(cmd[i]->outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
-				if (fd == -1)
-					exit_error("File not found");
-				dup2(fd, STDOUT_FILENO);
-				close(fd);
-			}
-			// ? Handle builtins in child process when in a pipeline
-			if (!ft_strncmp(cmd[i]->cmd_split[0], "echo", 4))
-			{
-				do_echo(cmd[i]);
-				exit(0);
-			}
-			if (!ft_strncmp(cmd[i]->cmd_split[0], "exit", 4))
-			{
-				do_exit(cmd[i]);
-				exit (1);
-			}
-			if (!ft_strncmp(cmd[i]->cmd_split[0], "cd", 2))
-			{
-				int rtn_code = do_cd(cmd[i], path);
-				if (rtn_code)
-					exit (rtn_code);
-				exit(0);
-			}
-			if (!ft_strncmp(cmd[i]->cmd_split[0], "export", 6))
-			{
-				export_handle(cmd[i], path);
-				exit(0);
-			}
-			if (!ft_strncmp(cmd[i]->cmd_split[0], "env", 3))
-			{
-				print_env(path);
-				exit(0);
-			}
-			// For external commands
-			setup_command(cmd[i], path);
-			execve(cmd[i]->cmd_path, cmd[i]->cmd_split, path->envp);
-			exit(127);
+			// Child process
+			setup_io_redirections_child(cmd[i], pipe_fd, prev_pipe, i);
+			execute_builtin_child(cmd[i], path);
+			// Never reaches here
 		}
-		// Parent closes previous pipe and manages current pipe
-		if (i > 0 && cmd[i-1]->has_pipe)
-		{
-			close(prev_pipe[0]);
-			close(prev_pipe[1]);
-		}
-		if (cmd[i]->has_pipe)
-		{
-			prev_pipe[0] = pipe_fd[0];
-			prev_pipe[1] = pipe_fd[1];
-		}
-		else
-		{
-			prev_pipe[0] = -1;
-			prev_pipe[1] = -1;
-		}
+
+		// Parent process
+		handle_pipes(pipe_fd, prev_pipe, cmd, i);
 	}
-	// Wait for all child processes to finish
-	while (waitpid(-1, &path->exit_status, 0) > 0)
-	{
-		if (WIFEXITED(path->exit_status))
-			path->exit_status = WEXITSTATUS(path->exit_status);
-		else if (WIFSIGNALED(path->exit_status))
-			path->exit_status = 128 + WTERMSIG(path->exit_status);
-	}
-	free_cmds(cmd);
+
+	wait_for_children(path);
 }
 
+static void wait_for_children(t_cmd_path *path)
+{
+	int status;
+
+	while (waitpid(-1, &status, 0) > 0)
+	{
+		if (WIFEXITED(status))
+			path->exit_status = WEXITSTATUS(status);
+		else if (WIFSIGNALED(status))
+			path->exit_status = 128 + WTERMSIG(status);
+	}
+}
