1e5442f1130e7676fcfe44188e36e5bd7c48b331
Hamzah <hamzahtaimour@gmail.com>
2025-04-06 16:07:13 +0300

    trying to fix PWD

--- srcs/pwd_handle.c
+++ srcs/pwd_handle.c
@@ -3,19 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   pwd_handle.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mdarawsh <mdarawsh@student.42amman.com>    +#+  +:+       +#+        */
+/*   By: hassende <hassende@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 03:55:12 by mdarawsh          #+#    #+#             */
-/*   Updated: 2025/03/10 01:37:33 by mdarawsh         ###   ########.fr       */
+/*   Updated: 2025/04/06 15:47:58 by hassende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	pwd_handle(void)
+static char	*find_pwd(t_cmd_path *path)
 {
-	char *path;
-	path = getcwd(NULL, 0);
-	printf("%s\n", path);
-	free(path);
+	char	*pwd;
+	int		i;
+
+	i = 0;
+	while (path->envp[i])
+	{
+		if (ft_strncmp(path->envp[i], "PWD=", 4) == 0)
+		{
+			pwd = ft_strdup(path->envp[i] + 4);
+			return (pwd);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+void	pwd_handle(t_cmd_path *path)
+{
+	char	*pwd;
+
+	pwd = find_pwd(path);
+	for (int i = 0; path->envp[i]; i++)
+	{
+		printf("%s\n", path->envp[i]);
+	}
+	if (!pwd)
+	{
+		perror("PWD not found");
+		return;
+	}
+	printf("%s\n", pwd);
+	free(pwd);
 }
