1af03d420a6b98ed2bad7e9e01dc489e89944644
Hamzah <hamzahtaimour@gmail.com>
2025-04-06 15:52:54 +0300

    should have pushed this as well

--- inc/minishell.h
+++ inc/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mdarawsh <mdarawsh@student.42amman.com>    +#+  +:+       +#+        */
+/*   By: hassende <hassende@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 14:24:48 by mdarawsh          #+#    #+#             */
-/*   Updated: 2025/03/26 13:35:05 by hassende         ###   ########.fr       */
+/*   Updated: 2025/04/06 15:29:45 by hassende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,7 +110,7 @@ void	setup_signals();
 void	exec_cmd(char *line_read, t_cmd_path *path);
 void	init_cmds(t_cmd **cmd, char *line);
 void	setup_command(t_cmd *cmd, t_cmd_path *path);
-void	pwd_handle(void);
+void	pwd_handle(t_cmd_path *path);
 void	export_handle(t_cmd *cmd ,t_cmd_path *path);
 void	do_echo(t_cmd *cmd);
 void	free_cmds(t_cmd **cmd);
