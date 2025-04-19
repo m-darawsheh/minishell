30ddcf89f250e1809b7cab0ff8e3b17a31e4ee26
Asendar1 <hamzahtaimour@gmail.com>
2025-04-19 15:47:22 +0300

    fixed exit when overflow (for int_64t)

--- srcs/exit.c
+++ srcs/exit.c
@@ -6,11 +6,38 @@
 /*   By: hassende <hassende@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 20:47:36 by hassende          #+#    #+#             */
-/*   Updated: 2025/04/17 20:47:45 by hassende         ###   ########.fr       */
+/*   Updated: 2025/04/19 15:46:44 by hassende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	check_overflow(const char *str)
+{
+	int					i;
+	unsigned long long	max;
+	unsigned long long	result;
+
+	i = 0;
+	max = 9223372036854775807;
+	result = 0;
+	if (str[i] == '-')
+	{
+		max++;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		if (result > max / 10
+			|| (result == max / 10 && (str[i] - '0') > max % 10))
+			return (1);
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (0);
+}
 
 static int	exit_isdigit(char *str)
 {
@@ -28,7 +55,7 @@ static int	exit_isdigit(char *str)
 
 int	do_exit(t_cmd *cmd)
 {
-	int	exit_code;
+	long long	exit_code;
 
 	if (cmd->cmd_split[1] == NULL)
 		exit(0);
@@ -37,14 +64,14 @@ int	do_exit(t_cmd *cmd)
 		ft_putstr_fd("minishell: exit: too many arguments\n", 2);
 		return (1);
 	}
-	if (!exit_isdigit(cmd->cmd_split[1]))
+	if (!exit_isdigit(cmd->cmd_split[1]) || check_overflow(cmd->cmd_split[1]))
 	{
 		ft_putstr_fd("minishell: exit: ", 2);
 		ft_putstr_fd(cmd->cmd_split[1], 2);
 		ft_putstr_fd(": numeric argument required\n", 2);
-		exit (255);
+		exit (2);
 	}
 	exit_code = ft_atoi(cmd->cmd_split[1]);
 	exit_code %= 256;
-	exit (exit_code);
+	exit ((int)exit_code);
 }
